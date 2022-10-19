ab302848135839484c058bbdfa1e89ce610fcb73
AbdullahiAli <abdullahi154@gmail.com>
2022-10-19 13:09:33 +0200

    Updated via the Open Science Framework

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import torch.nn.functional as F
 import time
 from typing import Dict
 
@@ -53,5 +54,28 @@ def L2Loss(x:torch.FloatTensor):
 def Linear(x:torch.FloatTensor):
     return x
 
+def parse_loss(args, terms):
+    if args == None:
+        return L1Loss, torch.tensor(0.0)
+    
+    pre, post, weights = terms
+    arg1, arg2 = args.split('_')
+    if arg1 == 'l1':
+        loss_fn = L1Loss
+    else:
+        loss_fn = L2Loss
+        
+    if arg2 == 'pre':
+        loss_arg = pre
+    elif arg2 == 'post':
+        loss_arg = post
+    else:
+        loss_arg = weights
+        
+    return loss_fn, loss_arg
+            
 def init_params(size_x, size_y):
     return ((torch.rand(size_x, size_y) * 2.) - 1. ) * np.sqrt(1. / size_x)
+
+def normalize(x, p=2.0, dim=1):
+    return F.normalize(x, p, dim)
