2281f8132f9562226612e8c815fa725c3dbac4ee
Tom Hume <twhume@gmail.com>
2023-10-23 17:09:36 -0700

    Tidied up imports, made seeds a 2-dimensional array (to avoid an error when running train_models.py), and create 10 instances of every network.

--- train_models.py
+++ train_models.py
@@ -1,7 +1,11 @@
-import torch
-
 import argparse
+import cifar
+import mnist
+import Network
 import random
+import torch
+from train import train
+
 parser = argparse.ArgumentParser(description='device')
 parser.add_argument('--i', type=str, help='Device index')
 args = parser.parse_args()
@@ -11,21 +15,13 @@ if torch.cuda.is_available():
     torch.set_default_tensor_type(torch.cuda.FloatTensor)
 else:
     DEVICE = 'cpu'
-
-
 print('Using {}'.format(DEVICE))
 
 INPUT_SIZE= 28*28
-
 BATCH_SIZE = 32
 SEQ_LENGTH = 10
 
 # dataset loaders
-import mnist
-
-import Network
-
-from train import train
 
 train_set, validation_set, test_set = mnist.load(val_ratio=0.0)
 
@@ -35,11 +31,13 @@ Create and train ten instances of energy efficient RNNs for MNIST
 n_instances = 10 # number of model instances
 #losses = [str(beta)+'beta'+'l1_postandl2_weights' for beta in [3708.0] ]
 losses = ['l1_pre', 'l1_post', [str(beta)+'beta'+'l1_postandl2_weights' for beta in [3708.0]][0]]
-#seeds = [[random.randint(0,10000) for i in range(n_instances)], \
-seeds = [[random.randint(0,10000) for i in range(n_instances)]]
+seeds = [[random.randint(0,10000) for i in range(n_instances)] for j in range(len(losses))]
+#seeds = [[random.randint(0,10000) for i in range(n_instances)]]
 # train MNIST networks
+
 for loss_ind, loss in enumerate(losses):
     for i in range(0, n_instances):
+        print("loss", loss_ind, "instance", i)
         net = Network.State(activation_func=torch.nn.ReLU(),
                 optimizer=torch.optim.Adam,
                 lr=1e-4,
@@ -48,7 +46,6 @@ for loss_ind, loss in enumerate(losses):
                 title="patterns_rev/seeded_mnist/mnist_net_"+loss,
                 device=DEVICE,
                 seed=seeds[loss_ind][i])
-        
       
         train(net,
               train_ds=train_set,
@@ -59,11 +56,8 @@ for loss_ind, loss in enumerate(losses):
               sequence_length=SEQ_LENGTH,
               verbose=False)
             
-           
-        
         # # save model
         net.save()
-
 
 """
 Create and train ten instances of energy efficient RNNs for CIFAR10
@@ -74,16 +68,12 @@ BATCH_SIZE = 32
 SEQ_LENGTH = 10
 LOSS_FN = 'l1_pre'
 
-import cifar
-
-
-
 training_set, validation_set, test_set = cifar.load(val_ratio=0.0, color=True)
 
 """
 Create and train ten instances of energy efficient RNNs with cifar 10
 # """
-N = 1 # number of model instances per seed
+N = 10 # number of model instances per seed
 
 seeds = [random.randint(0,10000) for i in range(N)]   
 
@@ -112,4 +102,3 @@ for i in range(N):
               )
         ## save model
         cifar_net.save()
-        
