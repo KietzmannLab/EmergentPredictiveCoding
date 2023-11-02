982a1a46f36e7b06eb4ea434693411c7f305c555
Tom Hume <twhume@gmail.com>
2023-11-01 19:37:46 -0700

    Just load and save a simple model name. Don't worry about trying to find the most recent in the directory.

--- ModelState.py
+++ ModelState.py
@@ -23,16 +23,7 @@ class ModelState:
         self.device = device
 
     def save(self):
-        filepath = "./models/" + self.title
-        if not os.path.isdir(os.path.dirname(filepath)):
-            os.makedirs(os.path.dirname(filepath), exist_ok=True)
-
-        files = []
-        for file in os.listdir(os.path.dirname(filepath)):
-            if re.search("^" + os.path.basename(filepath) + "-[0-9]+\.pth$", file):
-                files.append(file)
-        idx = len(files)
-        filepath = filepath + "-" + str(idx) + ".pth"
+        filepath = "./models/" + self.title +".pth"
 
         torch.save({
             "epochs": self.epochs,
@@ -42,15 +33,10 @@ class ModelState:
         }, filepath)
 
     def load(self, idx=None):
-        filepath = "./models/" + self.title
-        if idx is None:
-            files = []
-            for file in os.listdir(os.path.dirname(filepath)):
-                if re.search("^" + os.path.basename(filepath) + "-[0-9]+\.pth$", file):
-                    files.append(file)
-            idx = len(files) - 1
-
-        filepath = "./models/" + self.title + "-" + str(idx) + ".pth"
+        if (idx is None):
+            filepath = "./models/" + self.title +".pth"
+        else:
+            filepath = "./models/" + self.title +"_" + str(idx) + ".pth"
 
         state = torch.load(filepath, map_location=torch.device(self.device))
         self.epochs = state['epochs']
