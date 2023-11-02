af169781135233de8bd332b853846399017fdfff
Tom Hume <twhume@gmail.com>
2023-11-01 19:39:00 -0700

    Implement a concept of early stopping via a patience argument.

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import sys
 import torch
 from typing import Callable
 import functions
@@ -93,17 +94,34 @@ def train(ms: ModelState,
           num_epochs: int = 1,
           batch_size: int = 32,
           sequence_length: int = 3,
+          patience: int = 200,
           verbose = False):
     ms_name = ms.title.split('/')[-1]
+    best_epoch = 0; tries = 0
+    best_loss = sys.float_info.max
+    best_network = None
+
     for epoch in range(ms.epochs+1, ms.epochs+1 + num_epochs):
         print("Epoch {}, Lossfn {}".format(epoch, ms_name))
 
         train_loss, train_res, h = train_epoch(ms, train_ds, loss_fn, batch_size, sequence_length, verbose=verbose)
 
         test_loss, test_res = test_epoch(ms, test_ds, loss_fn, batch_size, sequence_length)
-        if epoch == 1 or epoch == num_epochs - 10:
-            W = ms.model.W.detach()
-            torch.save(W, 'models/'+ms.title+'W_'+ str(epoch)+'.pt')
+#        if epoch == 1 or epoch == num_epochs - 10:
+#            W = ms.model.W.detach()
+#            torch.save(W, 'models/'+ms.title+'W_'+ str(epoch)+'.pt')
         h, W_l1, W_l2 = functions.L1Loss(h),  functions.L1Loss(ms.model.W.detach()), functions.L2Loss(ms.model.W.detach())
         m_state = [[h.cpu().numpy()], [W_l1.cpu().numpy()], [W_l2.cpu().numpy()]]
         ms.on_results(epoch, train_res, test_res, m_state)
+
+        if (test_loss < best_loss):
+            best_loss = test_loss
+            best_epoch = epoch
+            best_network = ms.model.W.detach()
+            tries = 0
+        else:
+            print("Loss did not improve from", best_loss)
+            tries = tries + 1
+            if (tries >= patience):
+                print("Stopping early")
+                break;
